b2e72021b2bc9c59d36f41dca1cf5dcd8c232070
Muganwa <badpunter256@gmail.com>
2019-02-18 23:07:37 +0300

    sub contractor equipment management

--- src/app/components/SubContractorDetailsView/EquipmentTab/EquipmentTab.js
+++ src/app/components/SubContractorDetailsView/EquipmentTab/EquipmentTab.js
@@ -5,11 +5,16 @@ import { SecondarySelect, FmButton  } from 'components';
 import axios from 'axios';
 import 'extras/config';
 import { submit_styles } from './styles';
-import { dispatchedUserInfo, dispatchedSecondarySelectInfo } from 'extras/dispatchers';
+import { 
+    dispatchedUserInfo, 
+    dispatchedSecondarySelectInfo, 
+    dispatchedSubContractorsInfo 
+} from 'extras/dispatchers';
 import { equipmentCategories, equipmentCategoriesFull } from 'extras/config';
+import './equipmentTab.css';
 
 const baseURL = process.env.BACK_END_URL,
-userUpdateEndPoint = process.env.SUB_CONTRACTORS_UPDATE_END_POINT;
+userUpdateEndPoint = process.env.UPDATE_SUB_CONTRACTORS_END_POINT;
 
 const RenderEquipment = props => {
     let { currCat, onClose, id } = props;
@@ -18,7 +23,7 @@ const RenderEquipment = props => {
             Object.keys(currCat).map(key=>{
                 if(currCat[key]){
                     return (
-                        <div key={key}>
+                        <div className="equip-row" key={key}>
                             { equipmentCategoriesFull[id][key] }
                             <span 
                                 className="close right" 
@@ -60,13 +65,15 @@ class EquipmentTab extends React.Component{
     removeSubCategory = (e)=>{
         let userInfo = {...this.props.user},
         userId = userInfo.profileInfo.id,
-        equipment = {...userInfo.profileInfo.equipment},
+        subContractorsInfo = {...this.props.subContractorsInfo},
+        equipment = {...subContractorsInfo.currentSub.equipment},
         equipmentCategory = e.target.getAttribute('category'),
         url = baseURL + userUpdateEndPoint,
         equipmentName = e.target.getAttribute('subcategory');
         equipment[equipmentCategory][equipmentName] = false;
         userInfo.addEquipment.submitButton.isActive = false;
         this.props.dispatch(dispatchedUserInfo(userInfo));
+        this.props.dispatch(dispatchedSubContractorsInfo(subContractorsInfo));
         axios.post(url, {userId, sectTitle: "equipment", updateData: equipment}).then(res=>{
             if(res){
                 userInfo.addEquipment.submitButton.isActive = true;
@@ -110,10 +117,20 @@ class EquipmentTab extends React.Component{
                 selectedKey = key;
         });
         let secondaryOptions = equipmentCategoriesFull[selectedKey];
-        return new Promise((resolve, reject)=>{
+        return new Promise(resolve=>{
             let id = e.target.id,
             searchCategories = secondaryOptions,
             selectInfo = {...this.props.secondarySelect};
+            //check if equipment already selected
+            Object.keys(selectInfo.selectedOptions).map(key=>{
+                Object.keys(selectInfo.selectedOptions[key]).map(key1=>{
+                    let equipment = selectInfo.selectedOptions[key][key1];
+                    if(equipment === id){
+                        delete selectInfo.selectedOptions[key][key1];
+                    }
+                });
+            });
+            selectInfo.selectedOptions.push({[selectedKey]:id});
             selectInfo[categoryTitleAlt] = searchCategories[id];
             selectInfo[categoryTitleAltKey] = id;
             resolve(selectInfo);
@@ -125,31 +142,57 @@ class EquipmentTab extends React.Component{
         userInfo = {...this.props.userInfo},
         user = {...this.props.user},
         userId = userInfo.id,
-        equipment = {...userInfo.equipment},
+        subContractorsInfo = {...this.props.subContractorsInfo},
+        equipment = {...subContractorsInfo.currentSub.equipment},
         equipmentCategory = selectInfo.subEquipmentSelectedCategoriesKey,
         url = baseURL + userUpdateEndPoint,
         equipmentName = selectInfo.subEquipmentSelectedSubCategoriesKey;
         equipment[equipmentCategory][equipmentName] = true;
         user.addEquipment.submitButton.isActive = false;
-        this.props.dispatch(dispatchedUserInfo(user));
-        axios.post(url, {userId, sectTitle: "equipment", updateData: equipment}).then(res=>{
-            if(res){
+        if(equipment[equipmentCategory]){
+            //update equipment object according to selected options
+            Object.keys(selectInfo.selectedOptions).map(key=>{
+                Object.keys(selectInfo.selectedOptions[key]).map(key1=>{
+                    let equipmentAlt = selectInfo.selectedOptions[key][key1];
+                    equipment[key1][equipmentAlt] = true;
+                });
+            });
+
+            this.props.dispatch(dispatchedUserInfo(user));
+            this.props.dispatch(dispatchedSubContractorsInfo(subContractorsInfo));
+            
+            axios.post(url, {userId, sectTitle: "equipment", updateData: equipment}).then(res=>{
+                console.log(res.data)
+                if(res){
+                    user.addEquipment.submitButton.isActive = true;
+                    this.props.dispatch(dispatchedUserInfo(user));
+                    this.forceUpdate();
+                }   
+            }).
+            catch(err=>{
                 user.addEquipment.submitButton.isActive = true;
                 this.props.dispatch(dispatchedUserInfo(user));
                 this.forceUpdate();
-            }   
-        }).
-        catch(err=>{
-            user.addEquipment.submitButton.isActive = true;
-            this.props.dispatch(dispatchedUserInfo(user));
-            this.forceUpdate();
-            throw err;
-        });
+                throw err;
+            });
+        }
+    }
+
+    removeSelectedEquipment = (e)=>{
+        let position = e.target.getAttribute('pos'),
+        secondarySelect = {...this.props.secondarySelect};
+        secondarySelect.selectedOptions.splice(position,1);
+        this.props.dispatch(dispatchedSecondarySelectInfo(secondarySelect));
+        this.forceUpdate();
     }
 
     render(){
         let { equipment }= this.props,
+        selectedArr = this.props.secondarySelect.selectedOptions,
         detectionCount = [],
+        mechanicalCount = [],
+        hazardCount = [],
+        planningCount = [],
         portableCount = [],
         passiveCount = [],
         emergencyCount = [],
@@ -157,12 +200,33 @@ class EquipmentTab extends React.Component{
             detectionAndWarningSystem,
             portableFireFightingEquipment,
             passiveFireProtection,
+            mechanicalEquipment,
+            specialHazard,
+            emergencyPlanning,
             emergencyExitLighting
         } = equipment;
 
         Object.keys(detectionAndWarningSystem).map(key=>{
             if(detectionAndWarningSystem[key]){
                 detectionCount.push(key);
+            }
+        });
+
+        Object.keys(mechanicalEquipment).map(key=>{
+            if(mechanicalEquipment[key]){
+                mechanicalCount.push(key);
+            }
+        });
+
+        Object.keys(specialHazard).map(key=>{
+            if(specialHazard[key]){
+                hazardCount.push(key);
+            }
+        });
+
+        Object.keys(emergencyPlanning).map(key=>{
+            if(emergencyPlanning[key]){
+                planningCount.push(key);
             }
         });
 
@@ -193,7 +257,7 @@ class EquipmentTab extends React.Component{
                     <div className="heading">Equipment Licenses Available<div className="bottom-border"></div></div>
                     <div className="information equipment">
                         <div className="categories">
-                            {detectionCount.length>0
+                        {detectionCount.length>0
                             ?<div className="subCategories">
                                 <h3>Detection and Warning System</h3>
                                 <div className="body">
@@ -217,6 +281,14 @@ class EquipmentTab extends React.Component{
                                 </div>
                             </div>
                             :null}
+                            {mechanicalCount.length>0
+                            ?<div className="subCategories">
+                                <h3>Mechanical Equipment</h3>
+                                <div className="body">
+                                    <RenderEquipment id="mechanicalEquipment" currCat={mechanicalEquipment} onClose={ this.removeSubCategory } />
+                                </div>
+                            </div>
+                            :null}
                             {emergencyCount.length>0
                             ?<div className="subCategories">
                                 <h3>Emergency Exit Lighting</h3>
@@ -225,9 +297,53 @@ class EquipmentTab extends React.Component{
                                 </div>
                             </div>
                             :null}
+                            {hazardCount.length>0
+                            ?<div className="subCategories">
+                                <h3>Special Hazard Equipment</h3>
+                                <div className="body">
+                                    <RenderEquipment id="specialHazard" currCat={specialHazard} onClose={ this.removeSubCategory } />
+                                </div>
+                            </div>
+                            :null}
+                            {planningCount.length>0
+                            ?<div className="subCategories">
+                                <h3>Emergency Planning</h3>
+                                <div className="body">
+                                    <RenderEquipment id="emergencyPlanning" currCat={emergencyPlanning} onClose={ this.removeSubCategory } />
+                                </div>
+                            </div>
+                            :null}
                         </div>
                         <div className="add">
                                 <div className="heading">Add licensed Equipment <div className="bottom-border"></div></div>
+                                { selectedArr.length > 0?<div className="subCategories">
+                                    <h4>Selected Equipment</h4>
+                                    <div className="body">
+                                        { Object.keys(selectedArr).map(key=>{
+                                            let selectedArr = this.props.secondarySelect.selectedOptions,
+                                            selected = selectedArr[key];
+                                            return(
+                                                <div key={key}>
+                                                    { Object.keys(selected).map(key1=>{
+                                                        return (
+                                                            <div className="equip-row" key={key1}>
+                                                                { equipmentCategoriesFull[key1][selected[key1]] }
+                                                                <span 
+                                                                    className="close right" 
+                                                                    pos={ key }  
+                                                                    onClick={ this.removeSelectedEquipment } 
+                                                                    id="close"
+                                                                >
+                                                                    &#x2716;
+                                                                </span>
+                                                            </div>
+                                                        )
+                                                    }) }
+                                                </div>
+                                            )
+                                        }) }
+                                    </div>
+                                </div>: null }
                                 <div className="body">
                                     <SecondarySelect 
                                         categories = { equipmentCategories }
